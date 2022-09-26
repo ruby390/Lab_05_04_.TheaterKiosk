43e71a6cf17213a7a53309ae1ea33e9c5c82cc6c
cleoc <kimuraam@mail.uc.edu>
2022-09-26 14:38:27 -0400

    project init

--- src/theaterKiosk.java
+++ src/theaterKiosk.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class theaterKiosk {
-}
+public class theaterKiosk {
+        public static void main(String[] args) {
+            final int WRISTBAND = 21;
+            int customerAge = 45;
+            if(customerAge >= WRISTBAND)
+            {
+                System.out.println("You get a wristband!");
+            }
+        }
+    }
